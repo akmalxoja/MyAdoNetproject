6d7cf5dd15b77c979fe4adb49280305959437488
akmalxoja <akmalxojasharifxojayev@gmail.com>
2024-02-11 19:56:56 +0500

    Добавьте файлы проекта.

---
+++ MyAdoNetproject/Program.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql;
+
+namespace MyAdoNetproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionstring = "Server=127.0.0.1; Port=5432; Database=Mydb; User Id=postgres; Password=root";
+
+            Vazifalar vazifalar = new Vazifalar();
+           
+
+
+           string query = vazifalar.GetbyId("students", 2);
+
+            NpgsqlConnection connection = new NpgsqlConnection(connectionstring);
+
+            connection.Open();
+
+            NpgsqlCommand command = new  NpgsqlCommand (query,connection);
+
+
+           // Read qilish uchun
+           /* NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt32(0)}. {reader.GetString(1)}  {reader.GetInt32(2)}");
+
+            }*/
+
+
+
+
+           
+
+            connection.Close();
+
+
+
+        }
+       
+
+
+    }
+
+}
---
+++ MyAdoNetproject/Vazifalar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAdoNetproject
+{
+    public class Vazifalar
+    {
+       
+        public string Create_table(string table_name)
+        {
+
+            string query = $"create table {table_name} (id serial, name varchar(40), age int)";
+            return query;
+        }
+
+        public string Insert_one(string table)
+        {
+            string query = $"insert into {table} (name, age) values ('Jack', 50)";
+            Console.WriteLine(query);
+            return query;
+        }
+       
+       
+        
+        public string InsertMany(string table_name)
+        {
+            IList<Users> users = new List<Users>()
+            {
+                new Users() { name = "Akmalxoja", age = 20 },
+                new Users() { name = "Aziz", age = 30 },
+                new Users() { name = "Otabek", age = 18 },
+            };
+
+            string query = $"insert into {table_name}(name, age) values ";
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                query += $@"('{users[i].name}', {users[i].age}), ";
+            }
+
+            return query.Substring(0, query.Length - 2) + ';';
+        }
+
+        public string GetAll(string table_name)
+        {
+            
+            string query = $"select *from {table_name}";
+            return query;
+        }
+
+        public string GetbyId(string table_name, int id){
+            
+            string query = $"select *from {table_name} where id = {id}";
+            return query;
+
+        }
+
+
+
+
+
+
+    }
+}
